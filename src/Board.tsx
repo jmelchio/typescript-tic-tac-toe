4edf9b772b8081434ec7e959e1e5bb7eea73bf02
Joris Melchior <joris.melchior@gmail.com>
2018-11-25 21:20:13 -0500

    Initial checkin of the project.
    First working version of sample tic-tac-toe version translated to
    typescript

---
+++ src/Board.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import './App.css';
+import Square from './Square';
+
+class Board extends React.Component {
+
+  public render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+
+  private renderSquare(i: number) {
+    return <Square />;
+  }
+}
+
+export default Board;
---
+++ src/Square.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+
+class Square extends React.Component {
+  public render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+export default Square;
